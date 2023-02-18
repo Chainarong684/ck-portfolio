c6d88b7e5af3697b0836ed87f7da42c83ba1018c
CMBEER <9k.chainarong@gmail.com>
2023-02-18 20:25:21 +0700

    theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,25 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
+import { createContext, useState } from 'react'
+
+interface ThemeContext {
+  theme: string
+  toggleTheme: () => void
+}
+
+export const ThemeContext = createContext<ThemeContext | null>(null)
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [theme, setTheme] = useState('light')
+
+  const toggleTheme = () => {
+    setTheme((current) => (current === 'light' ? 'dark' : 'light'))
+  }
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      <Component {...pageProps} />
+    </ThemeContext.Provider>
+  )
 }
