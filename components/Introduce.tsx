a34ca87b5e2913130ac39c6567bbfffbae9baef0
K.Chainarong <9k.chainarong@gmail.com>
2025-04-09 18:01:22 +0700

    asd

--- components/Introduce.tsx
+++ components/Introduce.tsx
@@ -7,7 +7,7 @@ const Introduce = () => {
       <div className='flex flex-col items-center text-center'>
         <h4>Hi, my name is</h4>
         <h1>Chainarong Kieatmingmongkol</h1>
-        <h2 className='p-5'>A Front end and Back end Developer</h2>
+        <h2 className='p-5'>A Front end and Back end Developer.</h2>
         <Typography.Paragraph className='w-[500px] leading-relaxed'>
           <pre>
             “const = (Programmer or Developer) =&gt; it mean an organism that converts <u>caffeine</u> into code usually
